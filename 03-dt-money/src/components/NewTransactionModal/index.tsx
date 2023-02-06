02dc4378f5ebba0222923fa044f04b7fba41b7fd
Renan G. Silva <renan.amigos28@gmail.com>
2023-02-06 19:06:58 -0300

    feat: component lista transactions and modal add new transaction

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
-import { CloseButton, Content, Overlay } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -9,14 +15,25 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
         <CloseButton>
-            <X size={24} />
+          <X size={24} />
         </CloseButton>
 
         <form action="">
-            <input type="text" name="" id="" placeholder="Descrição" required />
-            <input type="number" name="" id="" placeholder="Preço" required />
-            <input type="text" name="" id="" placeholder="Categoria" required />
-            <button type="submit">Cadastrar</button>
+          <input type="text" name="" id="" placeholder="Descrição" required />
+          <input type="number" name="" id="" placeholder="Preço" required />
+          <input type="text" name="" id="" placeholder="Categoria" required />
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+          <button type="submit">Cadastrar</button>
+
         </form>
       </Content>
     </Dialog.Portal>
